c2a10764a3794be9807c78be6188d4eff35999e7
Digvijay Narayan <digvijayn1993@gmail.com>
2024-07-09 15:02:30 +0530

    changed professor model file

--- backend/models/professor_model.js
+++ backend/models/professor_model.js
@@ -1,44 +1,8 @@
 const mongoose = require('mongoose');
 
-const professorSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  designation: {
-    type: [String],
-    required: true,
-  },
-  campus: {
-    type: String,
-    required: true,
-  },
-  department: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  domains: {
-    type: [String],
-    default: [],
-  },
-  responsibilities: {
-    type: [String],
-    default: [],
-  },
-  education: {
-    type: [String],
-    default: [],
-  },
-  experience: {
-    type: [String],
-    default: [],
-  },
-});
+const professorSchema = new mongoose.Schema()
+  
 
-const Professor = mongoose.model('professor', professorSchema);
 
-module.exports = Professor;
+
+module.exports = mongoose.model('professor', professorSchema,'professor');
