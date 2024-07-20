50b265b11064cfc625a722db072ed978ad00a2a6
Fardeen <fardeenclan@gmail.com>
2024-07-20 22:29:12 +0530

    added the publications sorting based on name and year

--- frontend/pesu-research-portal/src/App.js
+++ frontend/pesu-research-portal/src/App.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Professors from './components/Professors';
-import ProfessorDetail from './components/ProfessorDetail';
-import Centres from './components/Centres';
-import ResearchSupport from "./components/ResearchSupport"
-import Patents from './components/Patents';
-import Signup from './components/Signup';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Professors from "./components/Professors";
+import ProfessorDetail from "./components/ProfessorDetail";
+import Centres from "./components/Centres";
+import ResearchSupport from "./components/ResearchSupport";
+import Patents from "./components/Patents";
+import Signup from "./components/Signup";
+import Conference from "./components/Conference";
 // import axios from "axios";
 
 // axios.defaults.withCredentials=true
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login/>} />
-                <Route path="/profile" element={<Profile/>} />
-                <Route path="/Professors" element={<Professors/>} />
-                <Route path="/getProfessorbyid/:id" element={<ProfessorDetail/>} />
-                <Route path="/centres" element={<Centres/>} />
-                <Route path="/research-support" element={<ResearchSupport/>} />
-                <Route path="/patent-process" element={<Patents/>} />
-                <Route path="/signup" element={<Signup/>} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/Professors" element={<Professors />} />
+        <Route path="/getProfessorbyid/:id" element={<ProfessorDetail />} />
+        <Route path="/centres" element={<Centres />} />
+        <Route path="/research-support" element={<ResearchSupport />} />
+        <Route path="/patent-process" element={<Patents />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/conference" element={<Conference />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ frontend/pesu-research-portal/src/components/Conference.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Conference = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Conference;
