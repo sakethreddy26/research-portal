122d078e4040562dff50b1d54cd896f24dce37e9
ndigvijay <digvijayn1993@gmail.com>
2024-10-15 11:15:31 +0530

    fixing cors error

--- backend/server.js
+++ backend/server.js
@@ -19,15 +19,10 @@ const connect_to_db=async()=>{
     }
 }
 app.use(cookieParser());
-app.use(cors())
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();  
-  }
-app.use(allowCrossDomain);
+app.use(cors({
+    origin: ['http://10.2.80.90:8080'], 
+    credentials: true
+}));
 app.use(express.json())
 
 
