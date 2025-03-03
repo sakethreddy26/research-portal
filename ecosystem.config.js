29ad0717aadb02dcf55bac328e8ff2542b4ce5a4
saketh reddy <psaketh26@gmail.com>
2025-03-04 00:31:30 +0530

    nginx config  commit

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'pesu-research-backend',
+      cwd: './backend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G'
+    }
+  ]
+}; 
